403338de573238e65c20458e6e4bbf8a2bd5346b
ThunderBird2678 <edwardthetrueblade2678@gmail.com>
2016-02-08 17:35:26 -0500

    First Comments
    Committing now because system requires reboot.

--- Problem Sets/Set1.java
+++ Problem Sets/Set1.java
@@ -1,99 +1,91 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import the Scanner that will be used for I/O operations
 
 public class Set1
 {
-  
-  static Scanner sc;
-  
-  public static void clearScreen()
+
+  static Scanner sc; // Initialize the Scanner
+
+  public static void interest() // Problem 1: Interest
   {
-    
-    System.out.flush();
-    
-  }  
-  
-  public static void interest()
-  {
-    
-    sc = new Scanner (System.in);
-    
-    int principal;
+
+    sc = new Scanner (System.in); // Define the Scanner object
+
+    int principal; // Set up variables
     double interestRate;
-    
-    clearScreen();
-    
+
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     System.out.println();
     System.out.println("        Welcome to the Interest program!");
     System.out.println();
     System.out.print("                Principal ($): ");
-    
-    principal = sc.nextInt();
-    
+
+    principal = sc.nextInt(); // Prompt for; recieve principal amount
+
     System.out.print("              Interest Rate (%) :");
-    
-    interestRate = sc.nextDouble();
-    
+
+    interestRate = sc.nextDouble(); // Prompt for; recieve interest rate
+
     System.out.println();
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    
-    double year1, year2;
-    
-    year1 = principal * (interestRate / 100);
-    
+
+    double year1, year2; // Set up variables
+
+    year1 = principal * (interestRate / 100); // Generate results
+
     year2 = (principal + year1) * (interestRate / 100);
-    
+
     System.out.println();
-    System.out.format("           Year 1 Interest: $%.2f%n", year1);
+    System.out.format("           Year 1 Interest: $%.2f%n", year1); // Output results with formatting
     System.out.format("           Year 2 Interest: $%.2f%n", year2);
-    
+
     // System.out.println("           Year 2 Interest:" + year2); (Debug statement without formatting)
-    
+
     System.out.println();
-    
+
   }
-  
-  public static void arithmetic()
+
+  public static void arithmetic() // Problem 2: Arithmetic
   {
-    
-    sc = new Scanner (System.in);
-    
-    int operand1, operand2;
-    
+
+    sc = new Scanner (System.in); // Define Scanner Object
+
+    int operand1, operand2; // Set up variables
+
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     System.out.println();
     System.out.println("      Welcome to the Arithmetic program!");
     System.out.println();
     System.out.print("                 Operand 1:");
-    
-    operand1 = sc.nextInt();
-    
+
+    operand1 = sc.nextInt(); // Prompt for; recieve operand 1
+
     System.out.print("                 Operand 2:");
-    
-    operand2 = sc.nextInt();
-    
+
+    operand2 = sc.nextInt(); // Prompt for; recieve operand 2
+
     System.out.println();
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     System.out.println();
-    System.out.println("                 " + operand1 + " + " + operand2 + " = " + (operand1 + operand2));
+    System.out.println("                 " + operand1 + " + " + operand2 + " = " + (operand1 + operand2)); // Simple calculations are simply output
     System.out.println("                 " + operand1 + " - " + operand2 + " = " + (operand1 - operand2));
     System.out.println("                 " + operand1 + " * " + operand2 + " = " + (operand1 * operand2));
-    
-    double divAnswer = (double)operand1 / (double)operand2;
-    
-    System.out.println("                 " + operand1 + " / " + operand2 + " = " + divAnswer);
+
+    double divAnswer = (double)operand1 / (double)operand2; // Utilizing floating point accuracy for division
+
+    System.out.println("                 " + operand1 + " / " + operand2 + " = "); // Output results
+    System.out.format("%.2f%n", divAnswer); // Output formatted quotient, rounded off to two decimal places
     System.out.println();
-    
+
   }
-  
+
   public static void receipt()
   {
-    
+
     sc = new Scanner (System.in);
-    
+
     double price;
     int quantity;
-    
+
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     System.out.println();
     System.out.println("      Welcome to the Receipt program!");
@@ -102,15 +94,15 @@ public class Set1
     System.out.println();
     System.out.println("           Enter Item Information:");
     System.out.println();
-    System.out.print("                Price: "); 
-    
+    System.out.print("                Price: ");
+
     price = sc.nextDouble();
-    
-    
-    System.out.print("               Quantity: "); 
-    
+
+
+    System.out.print("               Quantity: ");
+
     quantity = sc.nextInt();
-    
+
     System.out.println();
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     System.out.println();
@@ -120,22 +112,20 @@ public class Set1
     System.out.format(" x $%.2f", price);
     System.out.format(" = $%.2f%n", (quantity * price));
     System.out.println();
-    
-    
-    
+
+
+
   }
-  
+
   public static void main (String [] args){
-    
+
     sc = new Scanner (System.in);
-    
+
     int choice;
-    
+
     do
     {
-      
-      clearScreen();
-      
+
       System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       System.out.println();
       System.out.println("           Welcome to Problem Set 1");
@@ -154,51 +144,49 @@ public class Set1
       System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       System.out.println();
       System.out.print("                 Choice: ");
-      
+
       choice = sc.nextInt();
-      
+
       if(choice == 1)
       {
-        
+
         interest();
-        
+
       }
-      
+
       else if(choice == 2)
       {
-       
+
         arithmetic();
-        
+
       }
-      
+
       else if(choice == 3)
       {
-        
+
         receipt();
-        
+
       }
-      
+
       else if(choice != 0)
       {
-        
-        clearScreen();
-        
+
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println();
         System.out.println("          Please enter a valid command.");
         System.out.println();
-        
+
       }
-      
-      
+
+
     } while (choice != 0);
-    
+
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     System.out.println();
     System.out.println("             Farewell, good sir.");
     System.out.println();
     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-    
+
   }
-  
+
 }
