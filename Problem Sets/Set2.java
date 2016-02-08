726939da5444e39652cd093b644b9c5773a9bb2e
ThunderBird2678 <edwardthetrueblade2678@gmail.com>
2016-02-08 18:33:51 -0500

    Something happened
    Github is really picky, isn't it?

--- Problem Sets/Set2.java
+++ Problem Sets/Set2.java
@@ -13,6 +13,63 @@ public class Set2
 
   static Scanner sc; // Initalize the Scanner for I/O
 
+  public static void pizza()
+  {
+
+    int numPizzas; // Set up variables
+    char size;
+    String input;
+
+    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    System.out.println();
+    System.out.println("         Welcome to the Pizza program!");
+    System.out.println();
+    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    System.out.println();
+    System.out.print("             Number of Pizzas: ");
+
+    numPizzas = sc.nextInt(); // Prompt for; recieve number of Pizzas
+
+    sc.nextLine(); // Empty Scanner buffer
+
+    System.out.print("                Size (S,M,L): ");
+
+    size = sc.nextLine().charAt(0); // Prompt for; recieve size of pizzas
+
+    System.out.println();
+
+    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    System.out.println();
+
+    switch(size)
+    {
+
+      case 'S': case 's': // Ordering small pizzas
+
+        System.out.print("            " + numPizzas + " @ $6.99 = $");
+        System.out.format("%.2f%n", (6.99 * numPizzas)); // Calculates, returns cost (formatted to two decimal places)
+        System.out.println();
+        break; // Breaks out of case
+
+      case 'M': case 'm': // Ordering medium pizzas
+
+        System.out.print("            " + numPizzas + " @ $8.99 = $");
+        System.out.format("%.2f%n", (8.99 * numPizzas)); // Calculates, returns cost (formatted to two decimal places)
+        System.out.println();
+        break; // Breaks out of case
+
+      default: // Ordering large pizzas (or anything, really, since an invalid size is meant to result in a large pizza)
+
+        System.out.print("            " + numPizzas + " @ $10.99 = $");
+        System.out.format("%.2f%n", (10.99 * numPizzas)); // Calculates, returns cost (formatted to two decimal places)
+        System.out.println();
+        break; // Breaks out of case
+
+    }
+
+
+  }
+
   public static void main (String [] args)
   {
 
@@ -49,7 +106,7 @@ public class Set2
       if(choice == 1)
       {
 
-        // pizza(); // Question 1
+        pizza(); // Question 1
 
       }
 
