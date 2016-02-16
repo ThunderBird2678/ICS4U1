88d59bda3ddcdfd2fec61baebb064cfec0dd2da4
ThunderBird2678 <edwardthetrueblade2678@gmail.com>
2016-02-16 18:11:45 -0500

    Basics
    Main method set up, header created, spacing method copied over.
    
    Should be runnable already, though it doesn't do anything.

---
+++ Problem Sets/HK_Set4.java
@@ -0,0 +1,146 @@
+/*
+
+Problem Set 4
+Kai Huang
+16 February 2016
+
+*/
+
+import java.util.Scanner; // Import Scanner libraries
+
+public class HK_Set4
+{
+
+  static Scanner sc; // Initalize scanner object
+
+  public static void spacing( String printed ) // Custom method written in order to center - align output)
+  {
+
+    if(printed.length() > 72) // 72 is the length of my dividers, so I'm using that as my maximum value
+    {
+
+      System.out.println(printed); // print it directly if it's any larger
+
+
+    }
+
+    else // provided that it fits
+    {
+
+      for(int i = 0; i < (72 - printed.length())/2; i++) // the amount of spaces is simply 72 - length of my output, then divided by 2 to center it with equal spaces on either side
+      {
+
+        System.out.print(" "); // outputs the spaces
+
+      }
+
+      System.out.println(printed); // outputs my text; center - aligned
+
+    }
+
+  }
+
+  public static void spacing( String printed, int length ) // overloaded version that is used when I want in-line user input; int length is simply how long I believe the input will be (usually easily estimated)
+  {
+
+    if((printed.length() + length) > 72) // as above
+    {
+
+      System.out.println(printed);
+
+
+    }
+
+    else
+    {
+
+      for(int i = 0; i < (72 - printed.length() - length)/2; i++) // key thing here is that it also subtracts out a bit more to accomodate for user input
+      {
+
+        System.out.print(" ");
+
+      }
+
+      System.out.print(printed); // using .print() instead of .println() here because I want the input in-line
+
+    }
+
+  }
+
+  public static void main (String [] args)
+  {
+
+    sc = new Scanner (System.in); // Define Scanner Object
+
+    int choice; // Set up choice variable
+
+    do // Looping through this
+    {
+
+      System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      System.out.println();
+      spacing("Welcome to Problem Set 4");
+      spacing("Kai Huang ~ 16/02/2016");
+      System.out.println();
+      System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      System.out.println();
+      spacing("Choices");
+      System.out.println();
+      spacing("1: Invest2");
+      spacing("2: BankAccount");
+      spacing("3: HiLo");
+      System.out.println();
+      spacing("0: Exit");
+      System.out.println();
+      System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+      System.out.println();
+      spacing("Choice: ", 1);
+
+      choice = sc.nextInt(); // Read user's choice
+
+      System.out.println();
+
+      if(choice == 1)
+      {
+
+        //invest2; // Question 1
+
+      }
+
+      else if(choice == 2)
+      {
+
+        //bankAccount(); // Question 2
+
+      }
+
+      else if(choice == 3)
+      {
+
+        //hiLo(); // Question 3
+
+      }
+
+      else if(choice != 0) // Failsafe; warns user if they've entered a command out of bounds
+      {
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println();
+        spacing("Please enter a valid command.");
+        System.out.println();
+
+      }
+
+
+    } while (choice != 0); // Terminates if choice is 0
+
+    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    System.out.println();
+    spacing("Farewell, good sir."); // Termination message
+    System.out.println();
+    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+  }
+
+
+}
